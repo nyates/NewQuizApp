8f5e8d181480567046b3f346a8bddd97c7272004
nyates <nyates3@gatech.edu>
2018-10-09 09:37:37 -0400

    Added button functionality and toast

--- app/src/main/java/com/pattersonhs/nyates/newquizapp/QuizActivity.java
+++ app/src/main/java/com/pattersonhs/nyates/newquizapp/QuizActivity.java
@@ -2,12 +2,48 @@ package com.pattersonhs.nyates.newquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    TextView qTextView;
+    Button trueButton;
+    Button falseButton;
+    Button nextButton;
+    boolean correctAnswer = false;
+    int toastMessageID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        qTextView = (TextView) findViewById(R.id.question_text_view);
+        trueButton = (Button) findViewById(R.id.true_button);
+        falseButton = (Button) findViewById(R.id.false_button);
+        nextButton = (Button) findViewById(R.id.next_button);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean givenAnswer = true;
+                if (givenAnswer == correctAnswer)
+                {
+                    toastMessageID = R.string.correct_toast;
+                }
+                else
+                {
+                    toastMessageID = R.string.incorrect_toast;
+                }
+
+                Toast.makeText(
+                        QuizActivity.this, toastMessageID, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
     }
+
 }
